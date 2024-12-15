c6f4d8a98a6114b159a1c16043b45aef0edde538
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2024-12-15 20:36:47 +0300

    Добавил интерфейс IDamage, начал делать способность Shooter, сделал скрипт с его снарядом

---
+++ Assets/Scripts/Bullet/BulletShooter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class BulletShooter : MonoBehaviour
+    {
+        private int _damage;
+
+        public void SatDamage(int damage)
+        {
+            _damage = damage;
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            //Debug.Log(other.gameObject.name);
+            
+            if (other.gameObject.CompareTag("Enemy")) // Проверяем, что это враг
+            {
+                Debug.Log(other.gameObject.name);
+                // Обработка урона врагу
+                IDamage damageComponent = other.gameObject.GetComponent<IDamage>();
+                if (damageComponent != null)
+                {
+                    damageComponent.TakeDamage(_damage);
+                }
+
+                //Destroy(gameObject); // Уничтожаем пулю
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interfaces/IDamage.cs
@@ -0,0 +1,7 @@
+﻿namespace PixelSurvivor
+{
+    public interface IDamage
+    {
+        public void TakeDamage(int damage);
+    }
+}
