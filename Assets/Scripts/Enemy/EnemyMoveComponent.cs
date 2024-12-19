c8b110bb0b8c5d67c0544e900d7f642341e2396d
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2024-12-19 16:59:07 +0300

    начал делать контроллер EnemyMoveController

---
+++ Assets/Scripts/Enemy/EnemyMoveComponent.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class EnemyMoveComponent : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        private Transform _character;
+
+        private void OnEnable()
+        {
+            GameObject character = GameObject.FindGameObjectWithTag("Character");
+
+            if (character != null)
+            {
+                _character = character.transform;
+            }
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void FollowTarget()
+        {
+            if (_character != null)
+            {
+                float distance = Vector2.Distance(transform.position, _character.position);
+                
+                // Рассчитываем направление к цели
+                Vector3 direction = (_character.position - transform.position).normalized;
+                
+                direction.y = 0;
+                
+                // Обновляем позицию врага
+                transform.position += direction * _moveSpeed * Time.deltaTime;
+
+                // Проверяем, что направлению не равен нулевой вектор
+                if (direction != Vector3.zero)
+                {
+                    // Вычисляем поворот объекта, чтобы он смотрел в направлении движения
+                    Quaternion targetRotation = Quaternion.LookRotation(direction);
+            
+                    // Плавный поворот между текущей и целевой ротацией
+                    Quaternion newRotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, Time.fixedDeltaTime * _rotationSpeed);
+                    _rigidbody.rotation = newRotation;
+                }
+            }
+        }
+    }
+}
