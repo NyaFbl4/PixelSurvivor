3fb87f538906e3b9783d9c9ad9a02de5139252b1
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2024-12-21 22:55:14 +0300

    Добавил HealthComponent

---
+++ Assets/Scripts/Components/HeathComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class HeathComponent : MonoBehaviour, IDamage
+    {
+        [SerializeField] private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+
+            Debug.Log(_health);
+            
+            if (_health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
