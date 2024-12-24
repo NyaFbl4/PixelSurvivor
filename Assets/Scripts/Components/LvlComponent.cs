6d1409c70abaf867916a223618ea5921230542c4
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2024-12-24 12:18:25 +0300

    Добавил систему опыта, сдел префаб опыта

---
+++ Assets/Scripts/Components/LvlComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class LvlComponent : MonoBehaviour
+    {
+        [SerializeField] private int _lvl;
+        [SerializeField] private int _experience;
+
+        [SerializeField] private int[] _experienceLvl;
+
+        public void AddExperience(int newExperience)
+        {
+            _experience += newExperience;
+
+            _lvl = Array.FindLastIndex(_experienceLvl, e => _experience >= e);
+            
+            Debug.Log("lvl: " + _lvl.ToString() + " exp: " + _experience.ToString());
+        }
+
+        [ContextMenu("AddExp")]
+        void AddExp()
+        {
+            AddExperience(2);
+        }
+
+        public void UpdateLvl()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ExperiencePickup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class ExperiencePickup : MonoBehaviour
+    {
+        [SerializeField] private int _experience;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                LvlComponent lvlComponent = other.gameObject.GetComponent<LvlComponent>();
+
+                if (lvlComponent != null)
+                {
+                    lvlComponent.AddExperience(_experience);
+                }
+                
+                Destroy(gameObject);
+            }
+        }
+    }
+}
