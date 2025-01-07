32e4ce6e865729c02949bc5b3d945ff28296cbca
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-01-08 00:58:19 +0300

    Начал делать MVP для UI

---
+++ Assets/Scripts/UI/VIew/CurrencyView.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class CurrencyView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _currencyText;
+        private long _lastCurrency;
+
+        public void SetupCurrency(long currency)
+        {
+            Setter(currency);
+            _lastCurrency = currency;
+        }
+
+        public void UpdateCurrency(long currency)
+        {
+            _lastCurrency = currency;
+            Setter(_lastCurrency);
+        }
+
+        private void Setter(long value)
+        {
+            _currencyText.text = value.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/VIew/CurrencyViewProvider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class CurrencyViewProvider : MonoBehaviour
+    {
+        [SerializeField] private CurrencyView _experienceView;
+        [SerializeField] private CurrencyView _healthView;
+        [SerializeField] private CurrencyView _scoreView;
+
+        public CurrencyView ExperienceView => _experienceView;
+        public CurrencyView HealthView => _healthView;
+        public CurrencyView ScoreView => _scoreView;
+    }
+}
