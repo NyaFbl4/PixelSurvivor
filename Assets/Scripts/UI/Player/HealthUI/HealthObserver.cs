ae6ba5687c731b4d161a7965146c08a937bb79ac
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-01-09 17:27:50 +0300

    Добавил Storage и Observer для здоровья игрока

---
+++ Assets/Scripts/UI/Player/HealthUI/HealthObserver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Zenject;
+
+namespace PixelSurvivor
+{
+    public class HealthObserver : IInitializable, IDisposable
+    {
+        private readonly CurrencyView _view;
+        private readonly HealthStorage _storage;
+
+        public HealthObserver(HealthStorage storage, CurrencyView view)
+        {
+            _storage = storage;
+            _view = view;
+        }
+        
+        public void Initialize()
+        {
+            _storage.OnHealthChanget += OnHealthChanget;
+            _view.SetupCurrency(_storage.Health);
+        }
+
+        public void Dispose()
+        {
+            _storage.OnHealthChanget -= OnHealthChanget;
+        }
+
+        private void OnHealthChanget(int exp)
+        {
+            _view.UpdateCurrency(exp);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Player/HealthUI/HealthStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PixelSurvivor
+{
+    public class HealthStorage
+    {
+        public event Action<int> OnHealthChanget;
+        
+        public int Health { get; private set; }
+
+        public HealthStorage(int health)
+        {
+            Health = health;
+        }
+
+        public void AddHealth(int health)
+        {
+            Health += health;
+            OnHealthChanget?.Invoke(Health);
+        }
+
+        public void SpendHealth(int health)
+        {
+            Health -= health;
+            OnHealthChanget?.Invoke(Health);
+        }
+    }
+}
