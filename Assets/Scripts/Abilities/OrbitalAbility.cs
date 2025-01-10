ba23734f168edb7a7bfd499dc992a5462bc58bce
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-01-10 11:15:03 +0300

    Рефакторил скрипт OrbitelAbility, изменил корутину создания и вращения проджектайлов

--- Assets/Scripts/Abilities/OrbitalAbility.cs
+++ Assets/Scripts/Abilities/OrbitalAbility.cs
@@ -20,7 +20,7 @@ namespace PixelSurvivor
 
         private bool _isActive;
 
-        private List<GameObject> _projectiles;
+        [SerializeField] private List<GameObject> _projectiles;
 
         private void Start()
         {
@@ -50,11 +50,69 @@ namespace PixelSurvivor
 
         private void ActivateAbility()
         {
+            SpawnProjectiles();
             StartCoroutine(SpawnAndRotateProjectiles());
             _isActive = true;
         }
 
         private IEnumerator SpawnAndRotateProjectiles()
+        {
+            //_projectiles = new List<GameObject>();
+
+            //SpawnProjectiles();
+            
+            /*
+            for (int i = 0; i < _projectileCount; i++)
+            {
+                float angle = i * (360f / _projectileCount);
+                Vector2 projectilePosition = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), 
+                    Mathf.Sin(angle * Mathf.Deg2Rad)) * _radius;
+                
+                GameObject projectile = Instantiate(_projectile, 
+                    (Vector2)transform.position + projectilePosition, Quaternion.identity);
+
+                _projectiles.Add(projectile);
+
+                ProjectileOrbitalAbility projectileOrbitalAbilityComponent =
+                    projectile.GetComponent<ProjectileOrbitalAbility>();
+
+                if (projectile != null)
+                {
+                    projectileOrbitalAbilityComponent.SetDamage(_damage);
+                }
+            }
+            */
+            
+            float elapsedTime = 0f;
+            while (elapsedTime < _duration)
+            {
+                for (int i = 0; i < _projectileCount; i++)
+                {
+                    float angle = i * (360f / _projectileCount) + (elapsedTime / _duration * _rotationSpeed * 360f);
+                    Vector2 orbPosition = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * _radius;
+                    _projectiles[i].transform.position = (Vector2)transform.position + orbPosition;
+                }
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            /*
+            // Удаление шариков после завершения способности
+            foreach (var orb in _projectiles)
+            {
+                Destroy(orb);
+            }
+            _projectiles.Clear(); // Очистка списка
+            */
+            
+            DestroyProjectiles(_projectiles);
+            
+            _isActive = false;
+            StartCoroutine(AbilityCooldown());
+        }
+
+        private void SpawnProjectiles()
         {
             _projectiles = new List<GameObject>();
 
@@ -77,30 +135,19 @@ namespace PixelSurvivor
                     projectileOrbitalAbilityComponent.SetDamage(_damage);
                 }
             }
-            
-            float elapsedTime = 0f;
-            while (elapsedTime < _duration)
-            {
-                for (int i = 0; i < _projectileCount; i++)
-                {
-                    float angle = i * (360f / _projectileCount) + (elapsedTime / _duration * _rotationSpeed * 360f);
-                    Vector2 orbPosition = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * _radius;
-                    _projectiles[i].transform.position = (Vector2)transform.position + orbPosition;
-                }
 
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
+            //return projectiles;
+        }
 
+        private void DestroyProjectiles(List<GameObject> projectiles)
+        {
             // Удаление шариков после завершения способности
-            foreach (var orb in _projectiles)
+            foreach (var orb in projectiles)
             {
                 Destroy(orb);
             }
-            _projectiles.Clear(); // Очистка списка
-
-            _isActive = false;
-            StartCoroutine(AbilityCooldown());
+            
+            projectiles.Clear(); // Очистка списка
         }
     }
 }
