4be65b0707afe7616a8ee41658bfff221ce83be3
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-01-12 02:47:33 +0300

    Добавил PlayerHealthController

---
+++ Assets/Scripts/Units/Player/Components/PlayerHealthController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelSurvivor
+{
+    public class PlayerHealthController : MonoBehaviour, IDamage
+    {
+        [SerializeField] private int _health;
+        [SerializeField] private bool _isImmortal;
+        [SerializeField] private float _immortalDuration;
+        
+        public void TakeDamage(int damage)
+        {
+            if(_isImmortal)
+                return;
+
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                StartImmortalStatus(); 
+            }
+        }
+        
+        private void StartImmortalStatus()
+        {
+            _isImmortal = true;
+            StartCoroutine(EndImmortalStatus());
+        }
+        
+        private IEnumerator EndImmortalStatus()
+        {
+            yield return new WaitForSeconds(_immortalDuration); 
+            _isImmortal = false; 
+        }
+    }
+}
