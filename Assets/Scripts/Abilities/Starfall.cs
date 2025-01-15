655c4eb26fd135fcbad1fe2c693579f7646bc804
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-01-15 17:15:11 +0300

    Добавил анимации для Starfall

--- Assets/Scripts/Abilities/Starfall.cs
+++ Assets/Scripts/Abilities/Starfall.cs
@@ -1,7 +1,16 @@
-﻿namespace PixelSurvivor
+﻿using UnityEngine;
+
+namespace PixelSurvivor
 {
-    public class Starfall
+    public class Starfall : MonoBehaviour
     {
+        [SerializeField] private TargetTrackerComponent _targetTracker;
+        [SerializeField] private Transform _shootPoint;
         
+        [SerializeField] private int _projectileDamage;
+        [SerializeField] private int _maxCurrentShots;
+        
+        [SerializeField]  private float _projectileSpeed;
+        [SerializeField] private float _cooldown;
     }
 }
