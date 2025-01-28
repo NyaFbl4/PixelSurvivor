35b7dfba6ab7c8af88c2c37a17a520ecfe3d0d85
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-01-29 00:48:49 +0300

    Enemy
    Начал делать префабы новых enemy

--- Assets/Scripts/Units/Enemy/Spawner/EnemiesSpawner.cs
+++ Assets/Scripts/Units/Enemy/Spawner/EnemiesSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -15,6 +16,26 @@ namespace PixelSurvivor
         private Wave CurrentWave => _waves[_waveIndex];
         private bool HasWaves => _waveIndex < _waves.Length;
         
+        
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+            StartCoroutine(SpawnWaves());
+        }
+        
+        private IEnumerator SpawnWaves()
+        {
+            while (HasWaves)
+            {
+                yield return new WaitForSeconds(CurrentWave.spawnTime); // Ожидание времени волны
+                Spawn();
+                _waveIndex++;
+            }
+        }
+        
+        /*
         private void Update()
         {
             if (HasWaves && Time.timeSinceLevelLoad > CurrentWave.spawnTime)
@@ -23,6 +44,7 @@ namespace PixelSurvivor
                 _waveIndex++;
             }
         }
+        */
 
         private void Spawn()
         {
@@ -30,9 +52,46 @@ namespace PixelSurvivor
 
             for (int i = 0; i < count; i++)
             {
+                Vector3 spawnPosition = GetRandomSpawnPosition();
+                Instantiate(CurrentWave.enemyPrefab, spawnPosition, Quaternion.identity);
+            }
+            
+            /*
+            int count = Random.Range(CurrentWave.countMin, CurrentWave.countMax + 1);
+
+            for (int i = 0; i < count; i++)
+            {
                 var spawnPosition = _center.position + (Vector3)Random.insideUnitCircle* _spawnRadius;
                 Instantiate(CurrentWave.enemyPrefab, spawnPosition, Quaternion.identity);
             }
+            */
+        }
+        
+        private Vector3 GetRandomSpawnPosition()
+        {
+            // Получаем границы камеры
+            float cameraHeight = mainCamera.orthographicSize * 2;
+            float cameraWidth = cameraHeight * mainCamera.aspect;
+
+            // Генерируем позицию спавна вне области видимости камеры
+            Vector3 spawnPosition = Vector3.zero;
+            bool validPosition = false;
+
+            while (!validPosition)
+            {
+                spawnPosition = _center.position + (Vector3)Random.insideUnitCircle * _spawnRadius;
+
+                // Проверяем, находится ли позиция вне камеры
+                if (spawnPosition.x < (_center.position.x - cameraWidth / 2) || 
+                    spawnPosition.x > (_center.position.x + cameraWidth / 2) ||
+                    spawnPosition.y < (_center.position.y - cameraHeight / 2) || 
+                    spawnPosition.y > (_center.position.y + cameraHeight / 2))
+                {
+                    validPosition = true; // Позиция вне камеры валидная
+                }
+            }
+
+            return spawnPosition;
         }
     }
 }
