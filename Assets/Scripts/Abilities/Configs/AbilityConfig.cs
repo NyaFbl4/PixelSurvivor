1a1f41b801f26e35e62e20b9205167b0d8972dc0
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-04-09 15:09:26 +0300

    UI
    Изменил икони у способностей, поменял размер упгрейда способности

--- Assets/Scripts/Abilities/Configs/AbilityConfig.cs
+++ Assets/Scripts/Abilities/Configs/AbilityConfig.cs
@@ -5,7 +5,7 @@ namespace PixelSurvivor
     [CreateAssetMenu(fileName = "AbilityConfig")]
     public class AbilityConfig : ScriptableObject
     {
-        public Sprite Icon;
+        //public Sprite Icon;
         public GameObject PrefabAbility;
         //public Upgrade UpgradeAbility;
     }
--- Assets/Scripts/Abilities/Upgrade/Implementation/OrbitalAbilityUpgrade.cs
+++ Assets/Scripts/Abilities/Upgrade/Implementation/OrbitalAbilityUpgrade.cs
@@ -3,25 +3,4 @@
 namespace PixelSurvivor
 {
     public class OrbitalAbilityUpgrade : AbilityUpgrade<OrbitalAbility> { }
-    
-        /*
-        [SerializeField] private OrbitalAbility _orbitalAbilityPrefab;
-        
-        public override void Apply(GameObject playerAbilities)
-        {
-            var orbitalAbility = playerAbilities.GetComponentInChildren<OrbitalAbility>();
-
-            if (orbitalAbility == null)
-            {
-                orbitalAbility = Instantiate(_orbitalAbilityPrefab, playerAbilities.transform.position, Quaternion.identity);
-                orbitalAbility.transform.SetParent(playerAbilities.transform, true);
-            }
-            else
-            {
-                orbitalAbility.UpgradeAbility();
-            }
-
-        }
-    }
-    */
 }
