81033d1fa024b7e65f82bb83688b3edf346c42b7
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-04-11 16:36:21 +0300

    AbilitySystem
    Начал переписывать взаимодействие способностей с UI

---
+++ Assets/Scripts/UI/Player/NewAbilityUi/AbilitySlotProvider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PixelSurvivor.UI.Player.NewAbilityUi
+{
+    public class AbilitySlotProvider : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private GameObject _rootObject;
+        [SerializeField] private Image _displayImage; 
+        [SerializeField] private Image _cooldownImage; 
+        [SerializeField] private Image _borderImage;
+
+        public void Show() => _rootObject.SetActive(true);
+        public void SetDisplayImage(Sprite displayImage) => _displayImage.sprite = displayImage;
+        public void SetCooldownText(float current, float max) => _cooldownImage.fillAmount = current / max;
+        public void Hide() => _rootObject.SetActive(false);
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
