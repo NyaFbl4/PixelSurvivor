57fa026e6a639b6ab8076eca5c1dd025d5805699
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-04-21 00:37:55 +0300

    PlayerUI
    Начал переделывать UI персонажа

--- Assets/Scripts/Units/Player/Presenter/PlayerPresenter.cs
+++ Assets/Scripts/Units/Player/Presenter/PlayerPresenter.cs
@@ -1,7 +1,18 @@
-﻿namespace PixelSurvivor.Units.Player
+﻿using UnityEngine;
+
+namespace PixelSurvivor.Units.Player
 {
     public class PlayerPresenter
     {
         
+        
+        private ExpereinceManager _expereinceManager;
+        private HeathComponent _heathComponent;
+
+        public PlayerPresenter(ExpereinceManager expereinceManager, HeathComponent heathComponent)
+        {
+            _expereinceManager = expereinceManager;
+            _heathComponent = heathComponent;
+        }
     }
 }
