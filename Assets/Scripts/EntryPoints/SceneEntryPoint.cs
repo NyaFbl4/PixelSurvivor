4bc43dde8c6e19987f6d413e84e3c5a9aeefa0cd
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-05-04 20:22:02 +0300

    EntryPoint
    Добавил точку загрузки на сцену

---
+++ Assets/Scripts/EntryPoints/SceneEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PixelSurvivor.NewAbilitySystem.UI;
+using UnityEngine;
+
+namespace PixelSurvivor.EntryPoints
+{
+    public class SceneEntryPoint : MonoBehaviour
+    {
+        [SerializeField] private AbilityUpgradeManager _upgradeManager;
+
+        private void Start()
+        {
+            _upgradeManager.SuggestUpgrades();
+        }
+    }
+}
