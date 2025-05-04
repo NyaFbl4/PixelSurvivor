2a6ec3d96122fd4d2d8b32a870b0dfbaecb5e716
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-05-04 11:17:47 +0300

    GoldManager
    Начал делать механику валюты игрока для внутриигровых покупок

---
+++ Assets/Scripts/UI/MainMenu/ShopManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor.UI.MainMenu
+{
+    public class ShopManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _shop;
+
+        public void OpenShop()
+        {
+            _shop.SetActive(true);
+        }
+
+        public void CloseShop()
+        {
+            _shop.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Units/Player/Economy/GoldManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace PixelSurvivor.Units.Player.Economy
+{
+    public static class GoldManager
+    {
+        private const string GoldKey = "PlayerGold";
+
+        public static int Gold
+        {
+            get => PlayerPrefs.GetInt(GoldKey, 0);
+
+            set
+            {
+                PlayerPrefs.SetInt(GoldKey, value);
+                PlayerPrefs.Save();
+            }
+        }
+        
+        public static void AddGold(int amount)
+        {
+            Gold += amount;
+        }
+
+        public static bool SpendGold(int amount)
+        {
+            if (Gold >= amount)
+            {
+                Gold -= amount;
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
